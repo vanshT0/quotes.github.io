1f2b3d38b546dbcfa06e48a7af7bef0a8acc972f
Vansh <rajachoudhary697@gmail.com>
2021-11-07 22:09:19 +0530

    top | header

---
+++ src/app/outro/outro.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OutroComponent } from './outro.component';
+
+describe('OutroComponent', () => {
+  let component: OutroComponent;
+  let fixture: ComponentFixture<OutroComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ OutroComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OutroComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
